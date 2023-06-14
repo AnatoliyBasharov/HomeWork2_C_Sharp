04969b14e8e18facd5a2004c1aa4f216cf4ae37d
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-14 23:13:22 +0300

    Finish to HomeTask2

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+System.Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+int numberOne = Convert.ToInt32(number);
+Char[] array = numberOne.ToString().ToCharArray();
+
+if (numberOne < 100)
+{
+
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    System.Console.WriteLine(array[2]);
+}
