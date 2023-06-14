3fc4bb2a3217f1511a3403937c1e8e3de591032f
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-14 22:42:13 +0300

    Finish HomeTask1

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+System.Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+int numberOne = Convert.ToInt32(number);
+
+if(numberOne > 100 && numberOne < 1000){
+    System.Console.WriteLine("Число трехзначное");
+}else{
+    System.Console.WriteLine("Число не трехзначное");
+}
+
+Char[] array = numberOne.ToString().ToCharArray();
+System.Console.WriteLine($"Вторая цифра в числе {numberOne} является {array[1]}");
