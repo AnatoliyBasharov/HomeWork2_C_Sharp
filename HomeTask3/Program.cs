931d8e7f88cb094cecd7ce8f42bbecfd3bc6966d
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-14 23:31:42 +0300

    Finish HomeTask3

--- HomeTask3/Program.cs
+++ HomeTask3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+System.Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+int numberOne = Convert.ToInt32(number);
+
+if (numberOne > 5 && numberOne <= 7)
+{
+    System.Console.WriteLine("Является выходным днем");
+}
+if (numberOne > 0 && numberOne <= 5)
+{
+    System.Console.WriteLine("Это рабочий день, иди работай!");
+}
+if (numberOne > 7)
+{
+    System.Console.WriteLine("Такого дня не существует");
+}
